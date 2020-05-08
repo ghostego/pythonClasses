6198bbd133ff07e0f066863de251904b5d31b7df
Alex Johnson <ajohnson@zehnergroup.com>
2020-05-08 11:32:32 -0700

    section-3

---
+++ Section3/interpolation.py
@@ -0,0 +1,9 @@
+# this is Python 2 Interpolation, not really useful in modern projects
+age = 24
+print("My age is %d years" % age)
+
+major = "years"
+minor = "months"
+print("My age is %d %s, %d %s" % (age, major, 6, minor))
+print("PI is approximately %f" % (22 / 7))
+print("PI is approximately %60.50f" % (22 / 7))
--- Section3/strings.py
+++ Section3/strings.py
@@ -13,3 +13,10 @@ print(age)
 
 print(type(age))
 print(type(greeting))
+
+age_in_words = "2 years"
+# F-strings are like Javascript es6 syntax, using the f in front of the string
+print(name+f" is {age} years old")
+
+
+print(f"Pi is approximately {22/7:12.50f}")
